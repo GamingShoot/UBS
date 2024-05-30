07670c90a3862f6d63d08670ed991371a5bf3152
GamingShoot <43821163+GamingShoot@users.noreply.github.com>
2024-05-30 17:41:37 +0200

    Add files via upload

---
+++ admissionJS.js
@@ -0,0 +1,5 @@
+
+            function AffichageTexte() {
+                document.getElementById("myfooterJS").innerHTML = 
+                "L'ENSIBS devient la 5e École associée au réseau Polytech !";
+            }
